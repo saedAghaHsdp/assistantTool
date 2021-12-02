b0848ddf00e565ae1cbfa2b66ad644b4a55e6ef3
Adi Melling <adi.melling@philips.com>
2021-12-02 11:57:58 +0200

    Removed unnecessary files

--- RWSTroubleshooting/ClientApp/src/app/app.module.ts
+++ RWSTroubleshooting/ClientApp/src/app/app.module.ts
@@ -5,19 +5,11 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { TroubleshootComponent } from "./troubleshoot/troubleshoot.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     TroubleshootComponent
   ],
   imports: [
@@ -25,10 +17,8 @@ import { TroubleshootComponent } from "./troubleshoot/troubleshoot.component";
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'troubleshoot', component: TroubleshootComponent }
+      { path: '', component: TroubleshootComponent, pathMatch: 'full' },
+      //{ path: 'troubleshoot', component: TroubleshootComponent }
     ])
   ],
   providers: [],
